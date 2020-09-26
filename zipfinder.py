20d2325c710acc1f8e5be19a22d3ff953d67fbfd
Ryan <ryan.cartner@gmail.com>
2020-09-26 16:48:13 -0300

    Update zipfinder.py

--- zipfinder.py
+++ zipfinder.py
@@ -18,25 +18,26 @@ def parse_paths(line):
 	paths = []
 	full_path = ""
 	if len(line) > 0:
-		if "//" in line:
+		if "//" in line: # remove protocol://
 			line = "/".join(line.split("/")[3:])
-		if len(line) > 0 and "?" in line:
+		if len(line) > 0 and "?" in line: # remove any parameters
 			line = line.split("?")[0]
-		if len(line) > 0 and line[-1] == "/":
+		if len(line) > 0 and line[-1] == "/": #remove trailing slash
 			line = line[:-1]
-		for item in line.split("/"):
-			full_path = full_path + "/" + item
-			paths.append(full_path);
+		for item in line.split("/"): # add each path depth as its own item to paths
+			full_path = full_path + "/" + item # assemble full_path one directory at a time
+			paths.append(full_path); # add it to the the list
 		return paths
-
+		
 def generate_filenames(path):
 	filenames = []
-
 	for ext in extensions:
+		filenames.append(path + "." + ext)
+		if "." in path:
+			filename_noext = path.split(".")[0] + "." + ext
+			filenames.append(filename_noext)
 		if "." not in path:
-			filenames.append(path + "." + ext)
-		repeat_last_dir = path.split("/")[-1]
-		if "." not in repeat_last_dir:
+			repeat_last_dir = path.split("/")[-1]
 			filenames.append(path + "/" + repeat_last_dir + "." + ext)
 	return filenames
 
